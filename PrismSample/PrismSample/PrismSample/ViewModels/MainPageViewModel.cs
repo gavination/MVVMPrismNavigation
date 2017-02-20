73cd4c0cd3bc71efed570854d38365f5caca5671
Gavin Bauman <gabauman@microsoft.com>
2017-02-20 17:06:11 -0500

    Initial commit

---
+++ PrismSample/PrismSample/PrismSample/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,52 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PrismSample.ViewModels
+{
+    public class MainPageViewModel : BindableBase, INavigationAware
+    {
+        private string _title;
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        private string _buttonText;
+        public string ButtonText
+        {
+            get { return _buttonText; }
+            set {  SetProperty(ref _buttonText, value); }
+        }
+
+        private INavigationService _navigationService;
+        public DelegateCommand NavigateToSecondPageCommand { get; private set; }
+
+        public MainPageViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+            NavigateToSecondPageCommand = new DelegateCommand(NavigateToSecondPage);
+        }
+
+        private void NavigateToSecondPage()
+        {
+            _navigationService.NavigateAsync("SecondPage");
+        }
+
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatedTo(NavigationParameters parameters)
+        {
+            if (parameters.ContainsKey("title"))
+                Title = (string)parameters["title"] + " and Prism";
+            ButtonText = "Go to next page";
+        }
+    }
+}
---
+++ PrismSample/PrismSample/PrismSample/ViewModels/SecondPageViewModel.cs
@@ -0,0 +1,46 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PrismSample.ViewModels
+{
+    public class SecondPageViewModel : BindableBase
+    {
+
+        private string _label = "This is the second page";
+        public string Label
+        {
+            get { return _label; }
+            set { SetProperty(ref _label, value); }
+
+        }
+
+        private string _buttonText = "Go Back to the First Page";
+        public string ButtonText
+        {
+            get { return _buttonText; }
+            set { SetProperty(ref _buttonText, value); }
+        }
+
+        private INavigationService _navigationService;
+        public DelegateCommand NavigateToMainPageCommand { get; private set; }
+
+        public SecondPageViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+            NavigateToMainPageCommand = new DelegateCommand(NavigateToMainPage);
+        }
+
+        private void NavigateToMainPage()
+        {
+            _navigationService.GoBackAsync();
+
+        }
+
+
+
+    }
+}
